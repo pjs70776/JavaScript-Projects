6f4d5311e242fe405ffb5d86b81b24da22972e16
pjs70776 <patrick.j.stover.1985@gmail.com>
2024-05-31 13:51:25 -0700

    update

--- Project1_expressions_alert/JS/main.js
+++ Project1_expressions_alert/JS/main.js
@@ -1,17 +1,23 @@
 window.alert("Hello, World!");
-document.write('Hi');
-var A = "This is a string";
+document.write('Hi <br>');
+
+var A = "This is a string <br>";  //creating a window alert that displays a variable
 document.write(A);
 
-var B = (5,5);
+var B = (5,5);  //creating a window alert that displays a variable
 window.alert("B");
 
-document.write("Lisa told Bart, \"Knock it off, Bart! Or I'll tell dad!<br> \"Eat my shorts!\" Bart responded.");
+document.write("Lisa told Bart, \"Knock it off, Bart! Or I'll tell dad!<br> \"Eat my shorts!\" Bart responded.<br>"); //inserting quotation marks
 
-document.write("\"Be who you are and say what you feel," + "because those who mind don\'t matter and those who matter don\'t mind.\"" + "-Dr. Seuss");
+document.write("\"Be who you are and say what you feel," + "because those who mind don\'t matter and those who matter don\'t mind.\"" + "-Dr. Seuss <br>");
 
-var C = ("concatonated" + "string");
+var C = ("concatonated" + "string <br>");  //concatonating a string
 document.write(C)
+  
+var Family = "The Arezzinis", Dad = "Jeremiah", Mom = "Hermoine", Daughter = "Penny", Son = "Zorro";  
+document.write(Dad);  //selecting a variable and then printing it
 
-var Family = "The Arezzinis", Dad = "Jeremiah", Mom = "Hermoine", Daughter = "Penny", Son = "Zorro";
-document.write(Dad);
+document.write(3+3);  //performing a math function
+
+
+
---
+++ Project1_expressions_alert/JS/mainSubmission.js
@@ -0,0 +1,13 @@
+window.alert("Hello, World!");  //window alert message
+document.write('Document write message <br>');  //document message
+
+var Sent1 = "This is the beginning of the string <br>"; //creating a variable
+document.write(Sent1);
+var Sent2 = "and this is the end of the string <br>";  //creating a variable
+document.write(Sent2);
+
+var C = ("This is the beginning of the string" + " and this is the end of the string <br>");  //concatonating a string
+document.write(C)
+
+document.write(5+5);  //an expression
+
