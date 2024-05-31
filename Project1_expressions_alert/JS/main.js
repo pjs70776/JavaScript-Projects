217862265973c39a13a4679e500a00b977897cdd
pjs70776 <patrick.j.stover.1985@gmail.com>
2024-05-31 13:00:16 -0700

    update

---
+++ Project1_expressions_alert/JS/main.js
@@ -0,0 +1,17 @@
+window.alert("Hello, World!");
+document.write('Hi');
+var A = "This is a string";
+document.write(A);
+
+var B = (5,5);
+window.alert("B");
+
+document.write("Lisa told Bart, \"Knock it off, Bart! Or I'll tell dad!<br> \"Eat my shorts!\" Bart responded.");
+
+document.write("\"Be who you are and say what you feel," + "because those who mind don\'t matter and those who matter don\'t mind.\"" + "-Dr. Seuss");
+
+var C = ("concatonated" + "string");
+document.write(C)
+
+var Family = "The Arezzinis", Dad = "Jeremiah", Mom = "Hermoine", Daughter = "Penny", Son = "Zorro";
+document.write(Dad);
