4830290c6b37f8edd6efcba8df3f70788bc93b4f
pjs70776 <patrick.j.stover.1985@gmail.com>
2024-06-02 10:24:33 -0700

    update

--- Project2_functions/JS/main.js
+++ Project2_functions/JS/main.js
@@ -3,6 +3,6 @@ var B = "Corolla"  //assigning a variable
 
 function My_First_Function() {
     var str = "This is the button text!";
-    document.getElementById("Button_Text").innerHTML - str;
+    document.getElementById("Button_Text").innerHTML - str;  //a button with text that is called to display my function
 }
 
