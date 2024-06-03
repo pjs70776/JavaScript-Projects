9bbb754273a135348349628a193a1476a3def588
pjs70776 <patrick.j.stover.1985@gmail.com>
2024-06-03 15:00:03 -0700

    update

--- Project2_functions/JS/main.js
+++ Project2_functions/JS/main.js
@@ -1,14 +1,15 @@
 
-var A = "Civic"  //assigning a variable
+var A = "Toyota"  //assigning a variable
 var B = "Corolla"  //assigning a variable
 
 function My_First_Function() {
     var str = "This is the button text!";
-    document.getElementById("button").innerHTML - str;  //a button with text that is called to display my function
+    document.getElementById("button").innerHTML = str;  //a button with text that is called to display my function
+}
+
+function My_First_Function() {
+    A += B;
+    document.getElementById("button").innerHTML = A; 
 }
 
 
-
-
-let c = 2;
-console.log((c +=3)); 
