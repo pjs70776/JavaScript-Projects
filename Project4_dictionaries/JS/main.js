5bdb3bab6f5058e8ab5e5208b62ae9c0c03f72a0
pjs70776 <patrick.j.stover.1985@gmail.com>
2024-06-03 15:50:26 -0700

    update

--- Project4_dictionaries/JS/main.js
+++ Project4_dictionaries/JS/main.js
@@ -2,9 +2,10 @@ function my_Dictionary() {  //my dictionary
     var Animal = {
         Species:"Dog",         //different KVP's
         Color:"Black",
-        // deleted Animal:"Breed",
+        Breed:"Terrier", 
         Age:"9",
         Sound:"Bark"
     };
+    delete Animal.Breed;
     document.getElementById("Dictionary").innerHTML = Animal.Breed;
 }
