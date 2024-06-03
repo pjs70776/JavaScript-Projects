1304375c2877b107e86b2ba959ebcf4239dd7d84
pjs70776 <patrick.j.stover.1985@gmail.com>
2024-06-03 09:42:12 -0700

    update

---
+++ Project3_math_operations_folder/JS/main.js
@@ -0,0 +1,47 @@
+// function to compute the product of p1 and p2
+function addition_Function() {  
+    var addition = 2 + 2;
+    document.getElementById("Math").innerHTML ="2 + 2 = " + addition;  //addition function
+}
+
+function subtraction_Function() {
+    var Subtraction = 10 - 1;
+    document.getElementById("Math").innerHTML = "10 - 1" + Subtraction;  //subtraction function
+}
+
+function multiplication() {
+    var simple_Math = 6 * 8;
+    document.getElementById("Math").innerHTML = "6 x 8" = " + simple_Math;  //multiply function
+}
+
+function division() {
+    var simple_Math = 48 / 6;
+    document.getElementById("Math").innerHTML = "48 / 6" = " + simple_Math;  //division function
+}
+
+function more_Math() {
+    var simple_Math = (1 + 2) * 10 / 2 -5;  // modulus operator
+    document.getElementById("Math").innerHTML = "1 plus 2, multiplied by 10, divided in half and then subtracted by 5 equals " + simple_Math;
+}
+
+function modulus_Operator() {
+    var simple_Math = 25 % 6;  //unary operator
+    document.getElementById("Math").innerHTML = "When you divide 25 by 6 you have a remainder of: " + simple_Math;
+}
+
+function negation_Operator() {
+    var x =10;
+    document.getElementById("Math").innerHTML = -x;  // negation operator
+}
+
+var X = 5;
+ 	X++;
+ 	document.write(X);  //increment operator
+
+     varX = 5.25;
+ 	X--;
+ 	document.write(X);  //decrement operator
+
+window.alert(Math.random() * 100);  //random number between 1 and 100
+  
+
