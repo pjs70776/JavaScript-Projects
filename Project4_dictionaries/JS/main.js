63e32e4995e8a99151245c700f886309fb0240cc
pjs70776 <patrick.j.stover.1985@gmail.com>
2024-06-03 14:42:43 -0700

    updated

---
+++ Project4_dictionaries/JS/main.js
@@ -0,0 +1,10 @@
+function my_Dictionary() {  //my dictionary
+    var Animal = {
+        Species:"Dog",         //different KVP's
+        Color:"Black",
+        // deleted Animal:"Breed",
+        Age:"9",
+        Sound:"Bark"
+    };
+    document.getElementById("Dictionary").innerHTML = Animal.Breed;
+}
