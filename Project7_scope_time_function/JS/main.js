95eae35d30df861224c54be4690dba1a6db1a2cc
pjs70776 <patrick.j.stover.1985@gmail.com>
2024-06-04 13:34:11 -0700

    update

--- Project7_scope_time_function/JS/main.js
+++ Project7_scope_time_function/JS/main.js
@@ -50,3 +50,18 @@ function Age_Function() {
     document.getElementById("How_old_are_you?").innerHTML = Drink;
 }
 
+//else if statement
+function Time_function() {
+    var Time = new Date().getHours();
+    var Reply;
+    if (Time < 12 == Time > 0) {
+        Reply = "It is morning time!";
+    }
+    else if (Time >= 12 == Time < 18) {
+        Reply = "It is afternoon.";
+    }
+    else {
+        Reply = "It is evening time.";
+    }
+    document.getElementById("Time_of_day").innerHTML = Reply;
+}
