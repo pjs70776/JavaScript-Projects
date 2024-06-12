031fb799f3a1ddc4b20922bb9f6aa174000f69fd
pjs70776 <patrick.j.stover.1985@gmail.com>
2024-06-12 12:24:31 -0700

    update

--- Project10_loops_arrays/JS/main.js
+++ Project10_loops_arrays/JS/main.js
@@ -1,4 +1,4 @@
-//loop
+//while loop
 function Counting_to_Ten() {
     var Digit = "";
     var X = 1;
@@ -9,18 +9,11 @@ function Counting_to_Ten() {
     document.getElementById("Counting_to_Ten").innerHTML = Digit;
 }
 
-//length property
-let text = "Hello World!";
-let length = text.length;
-
-document.getElementById("length").innerHTML=length;
-
-
 //for loop
 var Instruments = ["Guitar", "Drums", "Piano", "Bass", "Violin", "Trumpet", "Flute"];
 var Content = "";
 var Y;
-function List_of_Instruments() {
+function for_Loop() {
     for (Y = 0; Y < Instruments.length; Y++) {
         Content += Instruments[Y] + "<br>";
     }
@@ -36,17 +29,12 @@ function array_Function() {
         Cat_Picture[1] + ".";
 }
 
-//constant
-function constant_function() {
-    const Musical_Instrument = {type:"guitar", brand:"Fender", color:"wood grain", year_made: "1969"};  //changed color from black to wood grain, added year_made.
-    Musical_Instrument.color = "wood grain";
-    Musical_Instrument.price = "$900";
-    document.getElementById("Constant").innerHTML = "The cost of the " + 
-        Musical_Instrument.year_made + "," + Musical_Instrument.color + " " +  //added .year_made and .color (changed)
-        Musical_Instrument.type + " was " + Musical_Instrument.price;
-   
-}
-
 //let
-
+var X = 82;
+document.write(X);
+{
+    let X = 33;
+    document.write("<br>" + X);
+}
+document.write("<br>" + X);
 
